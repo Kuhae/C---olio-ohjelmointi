2c542f394a621711e2b64e427b9ec977e3511583
Kuhae <franselmus@gmail.com>
2025-03-18 20:21:22 +0200

    Update HeroVsOrc.cs

--- HeroVsOrc.cs
+++ HeroVsOrc.cs
@@ -1,9 +1,6 @@
-﻿using System.Linq.Expressions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
 namespace ConsoleApp11
 {
-    internal class Program
+    internal class HeroVsOrc
     {
         static int playerHP = 20;
         static int orcHP = 20;
@@ -31,177 +28,225 @@ namespace ConsoleApp11
         {
             Random random = new Random();
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Blind Hero, defeat the blind orc ahead of you?");
+            Console.WriteLine(
+                "Urhea ritari, sinut on lähetetty tappamaan kylää kiusaava örkki." +
+                "\nLöydät örkin metsästä ja tämä hyökkää sinua kohti. Taitelu alkakoon!");
 
-            while (!gameOver)
+            while (!gameOver) // Pyörii niin kauan kunnes gameOver = true
             {
-                playerHittable = true;
+                // tekee molemmista osuttavia taas jos aikaisemmalla vuorolla joku onnistui puolustautua
+                playerHittable = true; 
                 orcHittable = true;
-                if (newPlayerHP <= 0 || newOrcHP <= 0)
-                { gameOver = true; }
+
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("----------------------------------------");
-                Console.WriteLine($"You: {newPlayerHP}/{playerHP}   Orc: {newOrcHP}/{orcHP}");
+                Console.WriteLine($"You: {newPlayerHP}/{playerHP}   Orc: {newOrcHP}/{orcHP}"); // Vuoron alun HP Pelaajalta ja örkiltä
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"1 - Attack \n" +
-                    $"2 - Defend");
+
+                // Mahdolliset actionit
+                Console.WriteLine(
+                    $"1 - Hyökkää \n" +
+                    $"2 - Puolusta"); 
+
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("What will you do?");
+                Console.WriteLine("Mitä aijot tehdä?");
                 Console.ForegroundColor = ConsoleColor.Blue;
+
                 try
                 {
-                    playerAction = Convert.ToInt32(Console.ReadLine());
+                    playerAction = Convert.ToInt32(Console.ReadLine()); // Lukee Pelaajan syöttämän tekstin ja converttaa Integer arvoksi
                 }
-                catch 
-                { 
-                    playerAction = 0;
+                catch
+                {
+                    playerAction = 0; // Jos pelaajan syöttämä teksti tuottaa errorin PlayerAction = 0
                 }
-                orcAction = random.Next(1,5);
-                if (orcAction != 2 || newOrcHP > orcHP*0.75f)
+
+                orcAction = random.Next(1, 5); // heittää noppaa yhden ja neljän välillä
+
+                if (orcAction != 2 || newOrcHP > orcHP * 0.75f) // käytännössä tekee niin että örkki ei ikinä puolusta jos hänen Hp on yli 75% alkuperäisestä Hpeesta
                 { orcAction = 1; }
-                
-                if (playerAction != 1 && playerAction != 2)
+
+                if (playerAction != 1 && playerAction != 2) // Jos pelaaja ei anna sopivaa vastausta hän osuu itseensä
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("You hit yourself...");
+                    Console.WriteLine("Löit itseäsi...");
                     Damage(player);
                 }
                 else
                 {
-                    if (playerAction == 2 && orcAction == 2)
+                    if (playerAction == 2 && orcAction == 2) // Jos molemmat puolustautuvat
                     {
-                        Console.ForegroundColor = ConsoleColor.Green
-                            ; Console.WriteLine("It seems you both tried to defend, nothing happens...");
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                        Console.WriteLine("Molemmat nostivat kilpensä, mitään ei tapahdu...");
                     }
                     else
                     {
-                        if (playerAction == 2) 
+                        if (playerAction == 2) // Jos pelaaja puolustautuu yritä puolustaa pelaajaa
                         { Defend(player); }
-                        if (orcAction == 2)
+                        if (orcAction == 2) // Jos örkki puolustautuu yritä puolustaa örkkiä
                         { Defend(orc); }
                     }
 
-                    if (orcAction == 1) 
+                    if (orcAction == 1) // Jos örkki hyökkää yritä vahingoittaa pelaajaa
                     { Damage(player); }
-                    if (playerAction == 1)
+                    if (playerAction == 1) // Jos pelaaja hyökkää yritä vahingoittaa örkkiä
                     { Damage(orc); }
                 }
-                if (newPlayerHP <= 0 || newOrcHP <= 0)
+
+                if (newPlayerHP <= 0 || newOrcHP <= 0) // Jos pelaajan HP tai örkin HP on 0 tai vähemmän peli päättyy
                 { gameOver = true; }
             }
-            if (newPlayerHP > newOrcHP)
+
+            EndScreen(); // Jos poistuu while loopista gameOverin vuoksi valitsee sopivan voitto näytön joko voitto sammakon tai hävitessä örkistä kuvan
+
+            static void Damage(int targetID)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine($"Congratulations you won!\n" +
-                    "          o  o   o  o\r\n" +
-                    "         |\\/ \\^/ \\/|\r\n" +
-                    "         |,-------.|\r\n" +
-                    "       ,-.(|)   (|),-.\r\n" +
-                    "       \\_*._ ' '_.* _/\r\n" +
-                    "        /`-.`--' .-'\\\r\n" +
-                    "   ,--./    `---'    \\,--.\r\n" +
-                    "   \\   |(  )     (  )|   /\r\n" +
-                    "    \\  | ||       || |  /\r\n" +
-                    "     \\ | /|\\     /|\\ | /\r\n" +
-                    "     /  \\-._     _,-/  \\\r\n" +
-                    "    //| \\\\  `---'  // |\\\\\r\n" +
-                    "   /,-.,-.\\       /,-.,-.\\\r\n" +
-                    "  o   o   o      o   o    o");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Blue; Console.WriteLine("You lost...\n" +
-                    "                                   ,----.__                         |\r\n" +
-                    "                                ,'        `.                       |\r\n" +
-                    "                            _  /            :                      ,-.\r\n" +
-                    "                           |.`:              :                    /  -\r\n" +
-                    "            ,'''''-._      | )               :                 _.'  --\r\n" +
-                    "           /         '.  _.`.   (88o    _    |_           _.-''      -\r\n" +
-                    "           |           `/    |   \"\"\"   9@8o  / )-..__._.-'      ,/'`-/\r\n" +
-                    "           ]     \\    ,:     `.         \"\"  :_/              ,-'  |\r\n" +
-                    "            :     \\-_/        `. `a,    ,   :              ,'    /\r\n" +
-                    "             `.    Y'       ,_  \\ \"7888\"  ,'   _.--''''---')     |\r\n" +
-                    "               \\ .'      _/'  `._\\      ,'---.<...        /     |\r\n" +
-                    "               .'      ,' '-.._   ':._,::...,'   /'     ,'      /\r\n" +
-                    "              /'     ,/        '`''''           /     ,'       /\r\n" +
-                    "             ,'    /  :                        /    ,'       ,-''''._\r\n" +
-                    "             |    ()   :                      |    |      .-'        '\r\n" +
-                    "             `.   :     ) __............____ .'    |_ .--'\r\n" +
-                    "              `.   `.  ,'                   `/       `'-.__\r\n" +
-                    "      _,....--'>    : /                     |   __...-._   `\\\r\n" +
-                    "   ,-'       .' |   `.                      \"--'        ` ._/'--._\r\n" +
-                    " ,'        /'    |   `.                                           '   \r\n" +
-                    "/         (.,   /|     :                                            \\.\r\n" +
-                    "             `'' :     :\\\r\n" +
-                    "                 )     :.:\r\n" +
-                    "                 : ; . ; '\r\n" +
-                    "                 '_: . '\r\n" +
-                    "                   '_:'");
-            }
+                Random random = new Random();
 
-        }
+                int number = random.Next(1, 7); // Vahingon suuruus
+                int Rng = random.Next(1, 101); // arvottu luku 1 ja 100 välillä
+                bool miss = false; // Säilöö tiedon jos vahingoitus epäonnistuu ohilyönnin vuoksi
 
-        static void Damage(int targetID)
-        {
-            Random random = new Random();
-            LiveDamageAmountPlayer = 0;
-            LiveDamageAmountOrc = 0;
-            int number = random.Next(1, 7);
-            int Rng = random.Next(1, 101);
-            bool miss = false;
+                if (Rng <= critChance) // jos arvottu luku osuu alle tai yhtäkuin critChancen, ja pienentää mahdollisuutta epäonnistua puolustautuessa
+                { 
+                    number += number / 2;
+                    failChance -= 2;
+                    Console.ForegroundColor = ConsoleColor.White; 
+                    Console.WriteLine("Crit!"); 
+                }
 
-            if (Rng <= critChance)
-            { number += number / 2; Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Crit!"); }
+                if (Rng <= missChance) // jos arvottu luku osuu alle tai yhtäkuin missChance
+                { miss = true; }
 
-            if (Rng <= missChance)
-            { miss = true; }
-
-            if (targetID == player && playerHittable) // Damage player
-            {
-                if (miss) { Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("You avoided the the attack!"); }
-                else
+                if (targetID == player && playerHittable) // yritetään vahingoittaa pelaajaa
                 {
-                    newPlayerHP -= number; 
-                    Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"You lost {number}HP...");
-                    LiveDamageAmountPlayer = number;
+                    if (miss) // jos örkki osuu ohi 
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                        Console.WriteLine("Väistit lyönnin!"); 
+                    }
+                    else // osuessa pelaajan Hpeeta vähennetään arvottu määrä ja päivitetään 
+                    {
+                        newPlayerHP -= number;
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine($"Menetit {number}HP...");
+                        LiveDamageAmountPlayer = number;
+                    }
+                }
+                else if (targetID == orc && orcHittable) // yritetään vahingoittaa örkkiä
+                {
+                    if (miss) // Jos pelaaja osuu ohi
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("Löit ohi..."); 
+                    }
+                    else // osuessa örkin Hpeeta vähennetään arvottu määrä ja säilötään paljonko vahinkoa tehtiin
+                    {
+                        newOrcHP -= number;
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                        Console.WriteLine($"Örkki menetti {number}HP!");
+                        LiveDamageAmountOrc = number; 
+                    }
                 }
             }
-            else if (targetID == orc && orcHittable) // Damage orc
+
+            static void Defend(int targetID)
             {
-                if (miss) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("You missed..."); }
-                else
+                Random random = new Random();
+                int Rng = random.Next(1, 101); // arvottu luku 1 ja 100 välillä
+                bool fail = false; // Säilöö tiedon jos puolustautuminen epäonnistuu
+                failChance += 5; // vaikeutaa puolustautumista joka kerta kun puolustaudut
+
+                if (Rng <= failChance) // Jos arvottu luku osuu alle failChancen
+                { fail = true; }
+
+                if (targetID == player) // Yritä puolustaa pelaajaa
                 {
-                    newOrcHP -= number;
-                    Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine($"The orc lost {number}HP!");
-                    LiveDamageAmountOrc = number;
+                    if (fail) // jos puolustautuminen epäonnistui
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("Kilpesi lipsahti kädestäsi..."); 
+                    }
+                    else // Jos puolustautuminen onnistui estä pelaajan vahingoittaminen
+                    {
+                        playerHittable = false;
+                        newPlayerHP += LiveDamageAmountPlayer; // Jos aikaisemmalla vuorolla örkki vahingoitti pelaajaa paranna pelaajan Hpeeta sen verran
+                        if (newPlayerHP > playerHP) // varmistaa ettei pelaajan Hp mene yli maximi Hp 
+                        { newPlayerHP = playerHP; }
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                        Console.WriteLine($"Puolustauduit kilvelläsi!");
+                    }
+                }
+                else if (targetID == orc) // Yritä puolustaa örkkiä
+                {
+                    if (fail) // jos epäonnistuu
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                        Console.WriteLine("Örkin kilpi oli väärässä kädessä!"); 
+                    }
+                    else // Jos onnistuu estä örkin vahingoittaminen
+                    {
+                        orcHittable = false;
+                        newOrcHP += LiveDamageAmountOrc; // Jos aikaisemmalla vuorolla pelaaja vahingoitti örkkiä paranna örkin Hpeeta vahingon määrällä
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("Örkki onnistui suojata itsensä...");
+                    }
                 }
             }
-        }
 
-        static void Defend(int targetID)
-        {
-            Random random = new Random();
-            int Rng = random.Next(1, 101);
-            bool fail = false;
-
-            if (Rng <= 30)
-            { fail = true; }
-
-            if (targetID == player) // Defend player
+            static void EndScreen() 
             {
-                if (fail) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("You failed to defend yourself..."); }
-                else
+                if (newPlayerHP > newOrcHP) // Jos Pelaaja voittaa piirtää voitto näytön
                 {
-                    playerHittable = false;
-                    newPlayerHP += LiveDamageAmountPlayer; Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine($"You successfully defended yourself");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(
+                        "Tapoit örkin!\n" +
+                        "          o  o   o  o\r\n" +
+                        "         |\\/ \\^/ \\/|\r\n" +
+                        "         |,-------.|\r\n" +
+                        "       ,-.(|)   (|),-.\r\n" +
+                        "       \\_*._ ' '_.* _/\r\n" +
+                        "        /`-.`--' .-'\\\r\n" +
+                        "   ,--./    `---'    \\,--.\r\n" +
+                        "   \\   |(  )     (  )|   /\r\n" +
+                        "    \\  | ||       || |  /\r\n" +
+                        "     \\ | /|\\     /|\\ | /\r\n" +
+                        "     /  \\-._     _,-/  \\\r\n" +
+                        "    //| \\\\  `---'  // |\\\\\r\n" +
+                        "   /,-.,-.\\       /,-.,-.\\\r\n" +
+                        "  o   o   o      o   o    o");
                 }
-            }
-            else if (targetID == orc) // Defend Orc
-            {
-                if (fail) { Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("The orc failed to defend himself!"); }
-                else
+                else // Jos pelaaja häviää piirtää örkistä kuvan
                 {
-                    orcHittable = false;
-                    newOrcHP += LiveDamageAmountOrc; Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("The orc successfully defended himself...");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(
+                        "Hävisit örkille...\n" +
+                        "                                   ,----.__                         |\r\n" +
+                        "                                ,'        `.                       |\r\n" +
+                        "                            _  /            :                      ,-.\r\n" +
+                        "                           |.`:              :                    /  -\r\n" +
+                        "            ,'''''-._      | )               :                 _.'  --\r\n" +
+                        "           /         '.  _.`.   (88o    _    |_           _.-''      -\r\n" +
+                        "           |           `/    |   \"\"\"   9@8o  / )-..__._.-'      ,/'`-/\r\n" +
+                        "           ]     \\    ,:     `.         \"\"  :_/              ,-'  |\r\n" +
+                        "            :     \\-_/        `. `a,    ,   :              ,'    /\r\n" +
+                        "             `.    Y'       ,_  \\ \"7888\"  ,'   _.--''''---')     |\r\n" +
+                        "               \\ .'      _/'  `._\\      ,'---.<...        /     |\r\n" +
+                        "               .'      ,' '-.._   ':._,::...,'   /'     ,'      /\r\n" +
+                        "              /'     ,/        '`''''           /     ,'       /\r\n" +
+                        "             ,'    /  :                        /    ,'       ,-''''._\r\n" +
+                        "             |    ()   :                      |    |      .-'        '\r\n" +
+                        "             `.   :     ) __............____ .'    |_ .--'\r\n" +
+                        "              `.   `.  ,'                   `/       `'-.__\r\n" +
+                        "      _,....--'>    : /                     |   __...-._   `\\\r\n" +
+                        "   ,-'       .' |   `.                      \"--'        ` ._/'--._\r\n" +
+                        " ,'        /'    |   `.                                           '   \r\n" +
+                        "/         (.,   /|     :                                            \\.\r\n" +
+                        "             `'' :     :\\\r\n" +
+                        "                 )     :.:\r\n" +
+                        "                 : ; . ; '\r\n" +
+                        "                 '_: . '\r\n" +
+                        "                   '_:'");
                 }
             }
         }
